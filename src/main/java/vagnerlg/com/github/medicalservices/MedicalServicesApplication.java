c9478f0dff88690fe0cdbea3c5760a73044459db
Vagner Gonçalves <vlgoncalves@gmail.com>
2023-09-07 18:06:05 -0300

    feat(string-boot): inical project medical services

---
+++ src/main/java/vagnerlg/com/github/medicalservices/MedicalServicesApplication.java
@@ -0,0 +1,13 @@
+package vagnerlg.com.github.medicalservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vagnerlg/com/github/medicalservices/MedicalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package vagnerlg.com.github.medicalservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
